ad79e8b8dc1d0f98cf64d1c6a4306fbf80bb1894
rick <109609345+riycksz@users.noreply.github.com>
2022-11-11 10:34:59 -0300

    Add files via upload

---
+++ JavaApplication28/src/javaapplication28/JavaApplication28.java
@@ -0,0 +1,22 @@
+
+package javaapplication28;
+
+import javax.swing.JOptionPane;
+public class JavaApplication28 {
+
+   
+    public static void main(String[] args) {
+    //crie um aplicativo que receba uma matriz n dimensional onde as duas primeiras 
+    //linhas recebem os gastos de um domicilio com agua
+    //até o mês de junho.
+    int v[][];
+    v = new int [2][6];
+    v [0][0]=0;
+    v [0][1]=1;
+    v [1][0]=2;
+    v [1][1]=3;
+    v [2][1]=4;
+    v [2][2]=5;
+    }
+    
+}
